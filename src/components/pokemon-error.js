396938a2882f6fe4f1fbc67cd9b996c213128ec6
VedantaMaha <vedanta.maha@zettabyte.sg>
2021-01-11 21:31:03 +0800

    implement error page

--- src/components/pokemon-error.js
+++ src/components/pokemon-error.js
@@ -1,6 +1,34 @@
+import pokemonSad from '../assets/pokemon-error.png'
+import { css, keyframes } from '@emotion/css'
+
+const loadingContainer = css({
+  width: '100%',
+  height: '300px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexFlow: 'column'
+})
+
+const infoText = css({
+  fontWeight: '700',
+  fontSize: '2rem',
+  color: 'rgba(0,0,0,.87)',
+  margin: '1rem'
+})
+
 function PokemonError() {
   return (
-    <div>failed to fetch data.</div>
+    <div className={loadingContainer}>
+      <img
+        src={pokemonSad}
+        className={css`
+          height: 150px;
+        `}
+        alt="Error occured"
+      />
+      <p className={infoText}>Oh no! An error occured!</p>
+    </div>
   )
 }
 
